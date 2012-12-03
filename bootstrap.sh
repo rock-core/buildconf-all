3b126463a58fae4d56fd93bc3e1489b88e304cec
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2012-12-03 09:39:12 +0100

    fix URL to autoproj_bootstrap and add ability to switch to http

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,10 +12,27 @@ if ! test -f $PWD/autoproj_bootstrap; then
         echo "download the following script yourself, and re-run this script"
         exit 1
     fi
-    $DOWNLOADER http://rock-robotics.org/autoproj_bootstrap
+    $DOWNLOADER http://rock-robotics.org/stable/autoproj_bootstrap
 fi
 
-ruby autoproj_bootstrap $@ git git://gitorious.org/rock/buildconf-all.git branch=master push_to=git@gitorious.org:rock/buildconf-all.git
+echo "Do you want to use the git protocol to access the build configuration?"
+echo "If the protocol is blocked by your network answer with no."
+
+# Check and interprete answer of "[y|n]"
+ANSWER=""
+until [ "$ANSWER" = "y" ] || [ "$ANSWER" = "n" ] 
+do
+	echo "Use git protocol? [y|n]"
+	read ANSWER
+	ANSWER=`echo $ANSWER | tr "[:upper:]" "[:lower:]"`
+done
+
+if [ "$ANSWER" = "y" ]; then
+    ruby autoproj_bootstrap $@ git git://gitorious.org/rock/buildconf-all.git push_to=git@gitorious.org:rock/buildconf.git branch=master
+else
+    ruby autoproj_bootstrap $@ git http://git.gitorious.org/rock/buildconf-all.git push_to=git@gitorious.org:rock/buildconf.git branch=master
+fi
+
 if test "x$@" != "xlocaldev"; then
     . $PWD/env.sh
     autoproj update
