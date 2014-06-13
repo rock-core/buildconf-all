cdccd9ad88ba739d9695c9585fa8cee66fd767e3
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2014-06-13 13:35:53 +0200

    moved rock-simulation to github

--- init.rb
+++ init.rb
@@ -25,7 +25,7 @@
 
 require 'autoproj/gitorious'
 Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
-env_add 'PATH', "/home/goldhoorn/bootstraps/all-rock/base/admin_scripts/bin"
+Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
 
 Autoproj.env_inherit 'CMAKE_PREFIX_PATH'
 
