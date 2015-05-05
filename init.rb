ee056a78d9e8bfddca15e3a5a6e08a6aa634b78e
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2015-05-05 10:14:36 +0200

    Removed gitorious link

--- init.rb
+++ init.rb
@@ -24,7 +24,6 @@
 #
 
 require 'autoproj/gitorious'
-Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
 Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
 
 Autoproj.env_inherit 'CMAKE_PREFIX_PATH'
